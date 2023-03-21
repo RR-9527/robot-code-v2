500ccd45731544f223b16546bc590362f0ff54b6
T-Lind <tiernanxklindauer@gmail.com>
2023-03-21 15:18:40 -0400

    updated tape detector auto to potentially display at judging

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcodekt/opmodes/auto/TapeDetectorAuto.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcodekt/opmodes/auto/TapeDetectorAuto.kt
@@ -3,31 +3,49 @@ package org.firstinspires.ftc.teamcodekt.opmodes.auto
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import ftc.rogue.blacksmith.Anvil
+import ftc.rogue.blacksmith.BlackOp
 import ftc.rogue.blacksmith.units.GlobalUnits
+import ftc.rogue.blacksmith.util.kt.invoke
 import ftc.rogue.blacksmith.util.toIn
+import org.firstinspires.ftc.teamcode.R
+import org.firstinspires.ftc.teamcode.pipelines.TapeDetector
+import org.firstinspires.ftc.teamcodekt.components.height
+import org.firstinspires.ftc.teamcodekt.components.meta.DeviceNames
+import org.firstinspires.ftc.teamcodekt.components.width
+import org.openftc.easyopencv.OpenCvCamera
+import org.openftc.easyopencv.OpenCvCameraFactory
+import org.openftc.easyopencv.OpenCvCameraRotation
 
 @Autonomous
-class TapeDetectorAuto : RogueBaseAuto() {
-    override val startPose = GlobalUnits.pos(0,0, 0)
+class TapeDetectorAuto : LinearOpMode() {
+    val tapeDetectorPipeline = TapeDetector(BlackOp.mTelemetry)
 
-    override fun mainTraj(startPose: Pose2d) =
-        Anvil.forgeTrajectory(bot.drive, startPose)
-            .back(0.01)
-//            .addTemporalMarker(-25) {
-////                updateTapeCorrection()
-//            }
-//            .addTemporalMarker(-15) {
-////                bot.drive.poseEstimate = (Pose2d(bot.drive.poseEstimate.x, bot.drive.poseEstimate.y+tapeCorrection.toIn(), bot.drive.poseEstimate.heading))
-//            }
+    val camera: OpenCvCamera
 
+    init {
+        camera = OpenCvCameraFactory.getInstance().createWebcam(
+            BlackOp.hwMap(DeviceNames.WEBCAM1),
+            R.id.cameraMonitorViewId,
+        )
+        camera.setPipeline(tapeDetectorPipeline)
 
+        camera.openCameraDeviceAsync(object : OpenCvCamera.AsyncCameraOpenListener {
+            override fun onOpened() {
+                camera.startStreaming(width, height, OpenCvCameraRotation.UPRIGHT)
+            }
 
-    private fun Anvil.resetBot() = this
-        .addTemporalMarker {
-            bot.arm.goToRest()
-            bot.wrist.setToRestingPos()
-            bot.lift.goToZero()
-            bot.claw.close()
+            override fun onError(errorCode: Int) {
+                throw RuntimeException("Error opening camera! Error code $errorCode")
+            }
+        })
+    }
+
+    override fun runOpMode() {
+        while(!opModeIsActive()){
+            telemetry.addData("Angle", tapeDetectorPipeline.tapeAngle)
+            telemetry.update()
         }
+    }
 }
